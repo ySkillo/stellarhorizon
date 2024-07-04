43b87b2042b659dbb26061d1932abca18b5db94d
ySkillo <matheusverdeazul40@gmail.com>
2024-07-04 20:40:34 -0300

    navbar e icons + animações feitas!

--- index.php
+++ index.php
@@ -1,12 +1,121 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/home/home.css">
+    <script src="https://kit.fontawesome.com/9b58b8faa9.js" crossorigin="anonymous"></script>
+    <title>StellarHorizon</title>
 </head>
-<body>
-    
-</body>
-</html>
 
+<body>
+    <header class="header">
+        <a href="#" class="logo">
+            <img src="./assets/img/Navbar/stellarhorizon5.png" alt="" class="logo-img">
+        </a>
+        <nav class="navbar">
+            <ul class="nav-items">
+                <li><a href="#" class="link">Inicio</a></li>
+                <li><a href="#" class="link">Sobre</a></li>
+                <li><a href="#" class="link">Serviços</a></li>
+                <li><a href="#" class="link">Equipe</a></li>
+                <li><a href="#" class="link">Projetos</a></li>
+                <li class="li-select">
+                    <select name="language" id="language-select" class="select-language">
+                        <option value="1" class="option">Português</option>
+                        <option value="2" class="option">Inglês</option>
+                        <option value="3" class="option">Espanhol</option>
+                    </select>
+                </li>
+            </ul>
+        </nav>
+        <a href="" class="contact-button">Contato</a>
+    </header>
+
+    <main class="main">
+        <div class="globo">
+            <div class="rotating-word">
+                <p class="--i">S</p>
+                <p class="--i">t</p>
+                <p class="--i">e</p>
+                <p class="--i">l</p>
+                <p class="--i">l</p>
+                <p class="--i">a</p>
+                <p class="--i">r</p>
+                <p class="--i">H</p>
+                <p class="--i">o</p>
+                <p class="--i">r</p>
+                <p class="--i">i</p>
+                <p class="--i">z</p>
+                <p class="--i">o</p>
+                <p class="--i">n</p>
+            </div>
+            <div class="orbit"></div>
+        </div>
+
+
+        <div class="box-social">
+            <div class="card-social">
+                <!-- <img src="" alt=""> -->
+                <a href="#" class="icon-social">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+
+                <div class="message-card-social">
+                    <p class="text-social">Rede social de contato da StellarHorizon conecte-se com:</p>
+                    <h4 class="name-social-i">
+                        Instagram
+                    </h4>
+                </div>
+
+            </div>
+            <div class="card-social">
+                <!-- <img src="" alt=""> -->
+                <a href="#" class="icon-social">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+
+                <div class="message-card-social">
+                    <p class="text-social">Rede social de contato da StellarHorizon conecte-se com:</p>
+                    <h4 class="name-social-f">
+                        Facebook
+                    </h4>
+                </div>
+            </div>
+            <div class="card-social">
+                <!-- <img src="" alt=""> -->
+                <a href="#" class="icon-social">
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
+
+                <div class="message-card-social">
+                    <p class="text-social">Rede social de contato da StellarHorizon conecte-se com:</p>
+                    <h4 class="name-social-t">
+                        Twitter
+                    </h4>
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <section class="section">
+
+    </section>
+    <section class="section">
+
+    </section>
+    <section class="section">
+
+    </section>
+    <section class="section">
+
+    </section>
+    <section class="section">
+
+    </section>
+
+
+</body>
+
+</html>
